fc6560b456367161973e13fbb5b1afaac2b9d35a
tak-matroid <tak@matroid.com>
2019-06-03 23:38:49 +0000

    fix varlen stack array in strftime_micro

--- libavutil/strftime_micro.c
+++ libavutil/strftime_micro.c
@@ -1,13 +1,20 @@
 #include "strftime_micro.h"
+#include "mem.h"
 
 size_t strftime_micro(char *buf, size_t size, const char *format, const struct timeval *tv)
 {
   struct tm *tm;
-  char temp_name[size];
+  char *temp_name = av_malloc(size);
+  if (temp_name == NULL)
+    return 0;
 
   if ((tm = localtime(&(tv->tv_sec))) == NULL)
     return 0;
 
   strftime(temp_name, size, format, tm);
-  return snprintf(buf, size, temp_name, tv->tv_usec);
+  size_t retval = snprintf(buf, size, temp_name, tv->tv_usec);
+
+  av_free(temp_name);
+  return retval;
 }
+
