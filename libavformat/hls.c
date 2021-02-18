b243cd14a71c5ea1e2bbd1947a4d91b011690213
tak-matroid <tak@matroid.com>
2021-02-18 14:53:14 -0800

    revert get_timebase comment

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -2086,8 +2086,8 @@ static void fill_timing_for_id3_timestamped_stream(struct playlist *pls)
 
 static AVRational get_timebase(struct playlist *pls)
 {
-    //if (pls->is_id3_timestamped)
-    //    return MPEG_TIME_BASE_Q;
+    if (pls->is_id3_timestamped)
+       return MPEG_TIME_BASE_Q;
 
     return pls->ctx->streams[pls->pkt.stream_index]->time_base;
 }
